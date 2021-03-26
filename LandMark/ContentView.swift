41fbfe16fec8e675b0cfa2259358f9378a42c3b3
Faiza Mazhar <faiza.mazhar@bbc.co.uk>
2021-03-26 11:30:10 +0000

    Initial Commit

---
+++ LandMark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMark
+//
+//  Created by Faiza Mazhar on 26/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by Faiza Mazhar on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
